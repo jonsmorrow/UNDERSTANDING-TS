afef3caa11b99338fb7d3de7a78c2a8105c7f6de
Jon Morrow <jon@morrowmail.com>
2024-10-16 14:50:36 -0700

    Function Return Types & "void"
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- app.ts
+++ app.ts
@@ -1,30 +1,11 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-    input1: Combinable, 
-    input2: Combinable, 
-    resultConversion: ConversionDescriptor
-) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+function add(n1: number, n2: number) {
+    return n1 + n2;
 }
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
+function printResult(num: number): void {
+    console.log('Result: ' + num);
+}
 
-const combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
+printResult(add(5, 12));
 
-const combineNames = combine('Jon', 'Anna', 'as-text');
-console.log(combineNames);
+// let someValue: undefined;
