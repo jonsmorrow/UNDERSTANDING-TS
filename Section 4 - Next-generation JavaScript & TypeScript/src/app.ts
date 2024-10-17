fd8e1b1ba8bc3b3627ab292ea88285894e16772b
Jon Morrow <jon@morrowmail.com>
2024-10-17 16:13:57 -0700

    "let" and "const"
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 4 - Next-generation JavaScript & TypeScript/src/app.ts
+++ Section 4 - Next-generation JavaScript & TypeScript/src/app.ts
@@ -1,1 +1,18 @@
-console.log('Time to get started...');
+const userName = 'Jon';
+// userName = 'Jon Morrow';
+
+let age = 42
+age = 40;
+
+function add(a: number, b: number) {
+    let result;
+    result = a + b;
+    return result;
+}
+
+// if (age > 20) {
+//     let isOld = true;
+// }
+
+// console.log(isOld);
+// console.log(result;
