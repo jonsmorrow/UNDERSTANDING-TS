67342b4f17f1d27f071676d125e9d17caf48a2e0
Jon Morrow <jon@morrowmail.com>
2024-10-17 20:47:02 -0700

    "private" and "public" Access Modifiers
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 5 - Classes & Interfaces/src/app.ts
+++ Section 5 - Classes & Interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;
+    public name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -8,12 +9,30 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        // validation
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee('Jon');
+accounting.addEmployee('Katie');
+
+// accounting.employees[2] = 'Cayman';
+
 accounting.describe();
+// accounting.name = 'NEW NAME';
+accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe }
 
-accountingCopy.describe();
+// accountingCopy.describe();
