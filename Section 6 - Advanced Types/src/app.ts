417fc752bb25a2d167d9baa8ac04f6f230edc830
Jon Morrow <jon@morrowmail.com>
2024-10-23 08:58:46 -0700

    Intersection Types
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 6 - Advanced Types/src/app.ts
+++ Section 6 - Advanced Types/src/app.ts
@@ -1,1 +1,21 @@
-console.log('Time to get started...');
+type Admin = {
+    name: string;
+    privileges: string[];
+}
+
+type Employee = {
+    name: string;
+    startDate: Date;
+}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Jon',
+    privileges: ['create-server'],
+    startDate: new Date();
+}
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
