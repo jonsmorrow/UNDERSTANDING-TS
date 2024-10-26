9d1932e7569bef30d38e63068d5006ef32281799
Jon Morrow <jon@morrowmail.com>
2024-10-26 13:03:06 -0700

    A First Class Decorator
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 8 - Decorators/src/app.ts
+++ Section 8 - Decorators/src/app.ts
@@ -1,1 +1,17 @@
-console.log('Time to get started...');
+function Logger(constructor: Function) {
+    console.log('Logging...');
+    console.log(constructor);
+}
+
+@Logger
+class Person {
+    name = 'Jon';
+
+    constructor() {
+        console.log('Creating person object...');
+    }
+}
+
+const pers = new Person();
+
+console.log(pers);
