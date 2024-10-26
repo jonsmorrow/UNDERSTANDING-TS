b64f6d74c4967266fa44373782aa092097ba35f6
Jon Morrow <jon@morrowmail.com>
2024-10-26 12:01:25 -0700

    Creating a Generic Function
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 7 - Generics/src/app.ts
+++ Section 7 - Generics/src/app.ts
@@ -1,13 +1,23 @@
-const names: Array<string> = []; // string[]
-// names[0].split(' ');
+// const names: Array<string> = []; // string[]
+// // names[0].split(' ');
 
-const promise: Promise<number> = new Promise((resolve, _reject) => {
-    setTimeout(() => {
-        resolve(10);
-    }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, _reject) => {
+//     setTimeout(() => {
+//         resolve(10);
+//     }, 2000);
+// });
 
-promise.then(data => {
-    // data.split(' ');
-    console.log(data);
-})
+// promise.then(data => {
+//     // data.split(' ');
+//     console.log(data);
+// })
+
+function merge<T extends {}, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+// console.log(merge({name: 'Jon'}, {age: 42}));
+const mergedObj = merge({name: 'Jon'}, {age: 42});
+const mergedObj2 = merge({name: 'Jon', hobbies: ['Flying', 'Scuba']}, {age: 42});
+console.log(mergedObj.name, mergedObj.age);
+console.log(mergedObj2.name, mergedObj2.age, mergedObj2.hobbies);
