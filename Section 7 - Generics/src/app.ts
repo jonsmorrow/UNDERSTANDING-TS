d99a3bc17244ff1248d2645753e4ced315ed48bd
Jon Morrow <jon@morrowmail.com>
2024-10-26 11:49:59 -0700

    Build-in Generics & What are Generics
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 7 - Generics/src/app.ts
+++ Section 7 - Generics/src/app.ts
@@ -1,1 +1,13 @@
-console.log('Time to get started...');
+const names: Array<string> = []; // string[]
+// names[0].split(' ');
+
+const promise: Promise<number> = new Promise((resolve, _reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 2000);
+});
+
+promise.then(data => {
+    // data.split(' ');
+    console.log(data);
+})
