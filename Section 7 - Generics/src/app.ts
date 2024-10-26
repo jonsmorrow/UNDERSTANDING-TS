e5a904f654015793791d6dc01ecf2f9f98aa6443
Jon Morrow <jon@morrowmail.com>
2024-10-26 12:44:44 -0700

    Generic Utility Types
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 7 - Generics/src/app.ts
+++ Section 7 - Generics/src/app.ts
@@ -82,3 +82,25 @@ const numberStorage = new DataStorage<number>();
 // // ...
 // objStorage.removeItem(jObj);
 // console.log(objStorage.getItems());
+ 
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(
+    title: string, 
+    description: string, 
+    date: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Jon', 'Katie'];
+// names.push('Cayman');
+// names.pop();
