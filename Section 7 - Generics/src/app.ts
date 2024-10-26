8f8b3850fa58c53754551c351515441f5967b72c
Jon Morrow <jon@morrowmail.com>
2024-10-26 12:04:46 -0700

    Working with Constraints
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 7 - Generics/src/app.ts
+++ Section 7 - Generics/src/app.ts
@@ -12,12 +12,12 @@
 //     console.log(data);
 // })
 
-function merge<T extends {}, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
 // console.log(merge({name: 'Jon'}, {age: 42}));
 const mergedObj = merge({name: 'Jon'}, {age: 42});
 const mergedObj2 = merge({name: 'Jon', hobbies: ['Flying', 'Scuba']}, {age: 42});
-console.log(mergedObj.name, mergedObj.age);
+console.log(mergedObj);
 console.log(mergedObj2.name, mergedObj2.age, mergedObj2.hobbies);
