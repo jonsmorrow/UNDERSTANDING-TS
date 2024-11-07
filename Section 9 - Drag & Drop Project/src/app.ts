7a9921bd3a72cac67c8257361a78b7fd4b27f0ea
Jon Morrow <jon@morrowmail.com>
2024-11-06 16:44:16 -0800

    Rendering Project Items with a Class
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 9 - Drag & Drop Project/src/app.ts
+++ Section 9 - Drag & Drop Project/src/app.ts
@@ -11,7 +11,6 @@ class Project {
     ) {}
 }
 
-
 // Project State Management
 type Listener<T> = (items: T[]) => void;
 
@@ -151,6 +150,27 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {}
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
+// ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
 
@@ -189,9 +209,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         listEl.innerHTML = '';
 
         for (const projItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = projItem.title;
-            listEl!.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, projItem);
         }
     }
 }
