d65fe3c59b39977aa716d6f88bf5241e5273e57e
Jon Morrow <jon@morrowmail.com>
2024-11-06 17:06:26 -0800

    Utilizing Interfaces to Implement Drag & Drop
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 9 - Drag & Drop Project/src/app.ts
+++ Section 9 - Drag & Drop Project/src/app.ts
@@ -1,3 +1,15 @@
+// Drag & Drop Interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project Type
 enum ProjectStatus { Active, Finished }
 
@@ -150,7 +162,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
     private project: Project;
 
     get persons() {
@@ -169,7 +181,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
-    configure() {}
+    @autoBind
+    dragStartHandler(event: DragEvent) {
+        console.log('DragStart');
+        console.log(event);
+    }
+    dragEndHandler(_: DragEvent) {
+        console.log('DragEnd');
+    }
+
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
+    }
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
@@ -220,7 +244,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
             new ProjectItem(this.element.querySelector('ul')!.id, projItem);
         }
     }
-}
+} 
 
 // ProjectInput Class
 class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
