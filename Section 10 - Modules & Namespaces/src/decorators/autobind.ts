5451df3a096e5a9b1509ecc680ec687b0892749a
Jon Morrow <jon@morrowmail.com>
2024-11-07 09:39:27 -0800

    Organizing Files & Folders
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

---
+++ Section 10 - Modules & Namespaces/src/decorators/autobind.ts
@@ -0,0 +1,18 @@
+namespace App {
+    // autobind decorator
+    export function autoBind(
+        _: any, 
+        _2: string, 
+        descriptor: PropertyDescriptor
+    ) {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFn = originalMethod.bind(this);
+                return boundFn;
+            }
+        }
+        return adjDescriptor;
+    }
+}
