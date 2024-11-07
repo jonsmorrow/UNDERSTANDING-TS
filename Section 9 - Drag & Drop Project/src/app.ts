aa3dffb9f9fc63e6d9ad0c1c32380ef8519d3807
Jon Morrow <jon@morrowmail.com>
2024-11-06 18:35:34 -0800

    Adding a Droppable Area
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 9 - Drag & Drop Project/src/app.ts
+++ Section 9 - Drag & Drop Project/src/app.ts
@@ -183,8 +183,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 
     @autoBind
     dragStartHandler(event: DragEvent) {
-        console.log('DragStart');
-        console.log(event);
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent) {
@@ -217,17 +217,21 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
 
     @autoBind
-    dragOverHandler(_event: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable');
+    dragOverHandler(event: DragEvent) {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable');
+        }
     }
 
-    dropHandler(_event: DragEvent) {
-        throw new Error("Method not implemented.");
+    @autoBind
+    dropHandler(event: DragEvent) {
+        console.log(event.dataTransfer!.getData('text/plain'));
     }
     
     @autoBind
-    dragLeaveHandler(_event: DragEvent) {
+    dragLeaveHandler(_: DragEvent) {
         const listEl = this.element.querySelector('ul')!;
         listEl.classList.remove('droppable');
     }
