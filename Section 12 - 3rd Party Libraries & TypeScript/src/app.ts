585713024c625900842a3e00557fa85c34d8db2e
Jon Morrow <jon@morrowmail.com>
2024-11-14 14:08:39 -0800

    No Types Needed: class-transformer
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 12 - 3rd Party Libraries & TypeScript/src/app.ts
+++ Section 12 - 3rd Party Libraries & TypeScript/src/app.ts
@@ -1,3 +1,23 @@
-import _ from 'lodash';
+import 'reflect-metadata';
+// import { plainToClass } from 'class-transformer'; // Deprecated
+import { plainToInstance } from 'class-transformer';
 
-console.log(_.shuffle([1,2,3]));
+import { Product } from './product.model';
+
+const products = [
+    { title: 'A Carpet', price: 29.99 },
+    { title: 'A Book', price: 10.99 }
+]
+
+// const p1 = new Product('A Book', 12.99);
+
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// })
+
+// const loadedProducts = plainToClass(Product, products); // Deprecated
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
