c1f585e288f5c7996a447a8947315dee186b4b77
Jon Morrow <jon@morrowmail.com>
2024-11-15 08:04:58 -0800

    TypeScript-embracing: class-validator
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 12 - 3rd Party Libraries & TypeScript/src/app.ts
+++ Section 12 - 3rd Party Libraries & TypeScript/src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 // import { plainToClass } from 'class-transformer'; // Deprecated
 import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 
 import { Product } from './product.model';
 
@@ -8,6 +9,16 @@ const products = [
     { title: 'A Carpet', price: 29.99 },
     { title: 'A Book', price: 10.99 }
 ]
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log('VALIDATION ERRORS!');
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
 
 // const p1 = new Product('A Book', 12.99);
 
